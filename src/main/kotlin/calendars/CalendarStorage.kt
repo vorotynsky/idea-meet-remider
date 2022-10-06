868950b05ad6ee7153093a0e21ad33aa7d477292
Maxim Vorotynsky <vorotynsky.maxim@gmail.com>
2022-10-07 02:15:04 +0300

    feat(space): load meets from space

--- src/main/kotlin/calendars/CalendarStorage.kt
+++ src/main/kotlin/calendars/CalendarStorage.kt
@@ -4,4 +4,13 @@ import calendars.space.SpaceCalendarProvider
 
 object CalendarStorage {
     val spaceCalendarProvider: SpaceCalendarProvider = SpaceCalendarProvider()
+    private var myEvents: List<CalendarItem> = listOf()
+
+    public var events: List<CalendarItem>
+        get() = myEvents
+        private set(value) { myEvents = value }
+
+    fun loadAll() {
+        myEvents = spaceCalendarProvider.load()
+    }
 }
--- src/main/kotlin/calendars/space/SpaceCalendarProvider.kt
+++ src/main/kotlin/calendars/space/SpaceCalendarProvider.kt
@@ -1,10 +1,12 @@
 package calendars.space
 
+import calendars.CalendarItem
 import calendars.CalendarProvider
 import com.intellij.credentialStore.CredentialAttributes
 import com.intellij.credentialStore.Credentials
 import com.intellij.credentialStore.OneTimeString
 import com.intellij.ide.passwordSafe.PasswordSafe
+import kotlinx.datetime.LocalDateTime
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
@@ -16,10 +18,38 @@ class SpaceCalendarProvider: CalendarProvider {
     override val isLoggedIn: Boolean
         get() = getCredentials() != null
 
-    override fun load() {
-        if (!isLoggedIn) { return }
+    override fun load() : List<CalendarItem> {
+        val url = getBaseUrl()
+        val token = getCredentials()
+        if (!isLoggedIn || (url == null || token == null)) { return listOf() }
 
-        TODO("Not yet implemented")
+        val profile = me(URL(url), token)?.id
+
+        val request = Request.Builder()
+            .url("$url/api/http/calendars/meetings?profiles=$profile")
+            .header("Authorization", "Bearer $token")
+            .header("Accept", "application/json")
+            .build()
+
+        val body = OkHttpClient().newCall(request).execute().use {
+            try {
+                if (it.isSuccessful && it.body != null) {
+                    val json = Json { ignoreUnknownKeys = true }
+                    json.decodeFromString<SpaceCalendarItemsDto>(it.body?.string() ?: "")
+                }
+                else null
+            } catch (e: Throwable) {
+                null
+            }
+        } ?: return listOf()
+
+        return body.data.map {
+            SpaceCalendarItem(
+                title = it.summary,
+                dateTime = LocalDateTime.parse(it.occurrenceRule.start.iso.removeRange(23, it.occurrenceRule.start.iso.length)),
+                url = url + "/meetings/" + it.id
+            )
+        }
     }
 
     private fun createCredentialAttributes(): CredentialAttributes {
@@ -27,6 +57,15 @@ class SpaceCalendarProvider: CalendarProvider {
     }
 
     internal fun login(url: URL, token: String, callback: (String?) -> Unit) {
+        val body = me(url, OneTimeString(token))
+
+        if (body?.username != null)
+            PasswordSafe.instance.set(createCredentialAttributes(), Credentials(url.toString(), token))
+
+        callback(body?.username)
+    }
+
+    private fun me(url: URL, token: OneTimeString): UsernameDto? {
         val request = Request.Builder()
             .url("$url/api/http/team-directory/profiles/me")
             .header("Authorization", "Bearer $token")
@@ -38,23 +77,22 @@ class SpaceCalendarProvider: CalendarProvider {
                 if (it.isSuccessful && it.body != null) {
                     val json = Json { ignoreUnknownKeys = true }
                     json.decodeFromString<UsernameDto>(it.body?.string() ?: "")
-                }
-                else null
+                } else null
             } catch (e: Throwable) {
                 null
             }
         }
-
-        if (body?.username != null)
-            PasswordSafe.instance.set(createCredentialAttributes(), Credentials(url.toString(), token))
-
-        callback(body?.username)
+        return body
     }
 
     private fun getCredentials() : OneTimeString? {
         return PasswordSafe.instance.get(createCredentialAttributes())?.password
     }
 
+   private fun getBaseUrl() : String? {
+        return PasswordSafe.instance.get(createCredentialAttributes())?.userName
+    }
+
     @Serializable
-    data class UsernameDto(val username: String)
+    data class UsernameDto(val id: String, val username: String)
 }
