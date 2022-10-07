7e6cb070d73ce8006f3062f6246f74d9d6adcaeb
Maxim Vorotynsky <vorotynsky.maxim@gmail.com>
2022-10-07 12:46:02 +0300

    feat: lock current file while meeting

--- src/main/kotlin/SetupBackground.kt
+++ src/main/kotlin/SetupBackground.kt
@@ -1,9 +1,13 @@
 import calendars.CalendarStorage
 import com.intellij.notification.Notification
 import com.intellij.notification.NotificationType
+import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vcs.changes.shelf.ShelveChangesManager.PostStartupActivity
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.impl.file.impl.FileManager
 import kotlinx.coroutines.*
 import kotlinx.datetime.*
 import kotlin.time.Duration
@@ -13,8 +17,8 @@ import kotlin.time.toKotlinDuration
 
 @OptIn(ExperimentalTime::class)
 class SetupBackground : PostStartupActivity() {
-    private val durationAll     = Duration.minutes(1)
-    private val durationSuccess = Duration.minutes(4)
+    private val durationAll     = Duration.seconds(1)
+    private val durationSuccess = Duration.seconds(4)
 
     @OptIn(ExperimentalTime::class, DelicateCoroutinesApi::class)
     override fun runActivity(project: Project) {
@@ -34,7 +38,7 @@ class SetupBackground : PostStartupActivity() {
 
                 println("new meets loaded, current count is ${CalendarStorage.events.size}")
 
-                delay(Duration.seconds(10))
+                delay(durationAll)
             }
         }
     }
@@ -54,11 +58,34 @@ class SetupBackground : PostStartupActivity() {
                 java.time.Duration.between(now.toJavaLocalDateTime(), minCalendarItem.startDateTime.toJavaLocalDateTime())
                     .toKotlinDuration()
 
+
             if (durationToEvent <= (durationAll + durationSuccess)) {
                 delay(durationToEvent)
                 Notification("Print", "Meet ${minCalendarItem.title} is started", NotificationType.INFORMATION).notify(
                     project
                 )
+
+                val list = mutableListOf<VirtualFile?>()
+
+                WriteCommandAction.runWriteCommandAction(project) {
+                    for (selectedFile in FileEditorManager.getInstance(project).selectedFiles) {
+                        selectedFile?.isWritable = false
+                        list.add(selectedFile)
+                    }
+                }
+
+
+                val durationToEnd =
+                    java.time.Duration.between(now.toJavaLocalDateTime(), minCalendarItem.finishDateTime.toJavaLocalDateTime())
+                        .toKotlinDuration()
+
+                delay(durationToEnd)
+
+                WriteCommandAction.runWriteCommandAction(project) {
+                    for (virtualFile in list) {
+                        virtualFile?.isWritable = false
+                    }
+                }
 
                 return true
             }
